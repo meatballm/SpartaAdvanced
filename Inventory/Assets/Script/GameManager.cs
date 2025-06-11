aa631a2a432868c2bd0b4785ebd0ba55555e11e0
meatballm <wh9555115@naver.com>
2025-06-11 09:59:53 +0900

    add : 인벤토리 아이템 추가 버튼 추가
    동적 스크롤 확인용

--- Inventory/Assets/Script/GameManager.cs
+++ Inventory/Assets/Script/GameManager.cs
@@ -11,16 +11,7 @@ public class GameManager : MonoBehaviour
     {
         Instance = this;
         player = new Charactor("Gigachad",50,50000,990,990,300,100);
-        AddItemToInventory("bomb");
-        AddItemToInventory("red");
-        AddItemToInventory("green");
-        AddItemToInventory("blue");
-        AddItemToInventory("yellow");
-        AddItemToInventory("goblin");
-        AddItemToInventory("skull");
-        AddItemToInventory("axe");
-        AddItemToInventory("golden");
-        AddItemToInventory("chest");
+        OnItemCheat();
     }
 
     public void AddItemToInventory(string itemId)
@@ -31,5 +22,18 @@ public class GameManager : MonoBehaviour
         else
             Debug.LogWarning($"ItemDatabase에 '{itemId}'가 없습니다.");
         UIInventory.instance.InitInventoryUI(player);
+    }
+    public void OnItemCheat()
+    {
+        AddItemToInventory("bomb");
+        AddItemToInventory("red");
+        AddItemToInventory("green");
+        AddItemToInventory("blue");
+        AddItemToInventory("yellow");
+        AddItemToInventory("goblin");
+        AddItemToInventory("skull");
+        AddItemToInventory("axe");
+        AddItemToInventory("golden");
+        AddItemToInventory("chest");
     }
 }
--- SprataATM/Assets/Script/SaveManager.cs
+++ SprataATM/Assets/Script/SaveManager.cs
@@ -11,25 +11,4 @@ public class SaveManager : MonoBehaviour
         savePath = "Assets\\Data\\userdata.json";
         instance = this;
     }
-
-    public void Save(UserData data)
-    {
-        string json = JsonUtility.ToJson(data, prettyPrint: true);
-        File.WriteAllText(savePath, json);
-        Debug.Log("저장 완료: " + savePath);
-    }
-
-    public UserData Load()
-    {
-        if (!File.Exists(savePath))
-        {
-            Debug.LogWarning("저장된 파일이 없습니다.");
-            return null;
-        }
-
-        string json = File.ReadAllText(savePath);
-        UserData data = JsonUtility.FromJson<UserData>(json);
-        Debug.Log("로드 완료");
-        return data;
-    }
 }
